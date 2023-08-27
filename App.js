7d657b003002d380b9fc41cab8cad82844c287fd
Brian Gelvez <briannn97@gmail.com>
2023-08-27 06:10:10 -0300

    app React Native

--- App.js
+++ App.js
@@ -1,20 +1,146 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import Button from './components/Button';
+import CircleButton from "./components/CircleButton";
+import IconButton from "./components/IconButton";
+import ImageViewer from './components/ImageViewer';
+import EmojiPicker from "./components/EmojiPicker";
+import EmojiList from "./components/EmojiList";
+import EmojiSticker from "./components/EmojiSticker";
+import * as ImagePicker from 'expo-image-picker';
+import { captureRef } from 'react-native-view-shot';
+import * as MediaLibrary from 'expo-media-library';
+import { useState, useRef } from "react";
+const PlaceholderImage = require("./assets/images/background-image.png");
 
 export default function App() {
+
+  const imageRef = useRef();
+  const [selectImage, setSelectImage] = useState(null);
+  const [showAppOptions, setShowAppOptions] = useState(false);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [pickedEmoji, setPickedEmoji] = useState(null);
+  const [status, requestPermission] = MediaLibrary.usePermissions();
+
+  if (status === null) {
+    requestPermission();
+  }
+
+  const onReset = () => {
+    setShowAppOptions(false);
+  }
+
+  const onAddSticker = () => {
+    setIsModalVisible(true);
+  }
+
+  const onModalClose = () => {
+    setIsModalVisible(false);
+  };
+
+  const onSaveImageAsync = async () => {
+    try {
+      const localUri = await captureRef(imageRef, {
+        height: 440,
+        quality: 1,
+      });
+
+      await MediaLibrary.saveToLibraryAsync(localUri);
+      if (localUri) {
+        alert("Imagen guardad con exito!");
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true, //sirve para recortar la imagen
+      quality: 1,
+    })
+    if (!result.canceled) {
+      setSelectImage(result.assets[0].uri);
+      setShowAppOptions(true);
+    } else {
+      alert('no seleccionaste la imagen pa');
+    }
+  }
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <GestureHandlerRootView style={styles.container}>
+      <View style={styles.imageContainer}>
+        <View ref={imageRef} collapsable={false}>
+        <ImageViewer placeholderImageSource={PlaceholderImage} selectImage={selectImage} />
+        {pickedEmoji !== null ? <EmojiSticker imageSize={40} stickerSource={pickedEmoji} /> : null}
+        </View>
+
+      </View>
+      {showAppOptions ? (
+        <View style={styles.optionsContainer}>
+          <View style={styles.optionsRow}>
+            <IconButton icon="refresh" label="Reset" onPress={onReset} />
+            <CircleButton onPress={onAddSticker} />
+            <IconButton icon="save-alt" label="Save" onPress={onSaveImageAsync} />
+          </View>
+        </View>
+      ) : (
+        <View style={styles.footerContainer}>
+          <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
+          <Button label="Use this photo" onPress={() => setShowAppOptions(true)} />
+        </View>
+      )}
+      <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+        <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
+      </EmojiPicker>
       <StatusBar style="auto" />
-    </View>
+    </GestureHandlerRootView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#305373',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  imageContainer: {
+    flex: 1, // Cambiar el valor de flex para hacer que la imagen sea más grande
+    paddingTop: 2, // Ajustar el espacio superior para mover la imagen más abajo
+    justifyContent: 'center', // Alinear la imagen verticalmente en el centro
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
+  },
+  optionsContainer: {
+    position: 'absolute',
+    bottom: 80,
+  },
+  optionsRow: {
+    alignItems: 'center',
+    flexDirection: 'row',
   },
 });
+
+
+//objeto result tiene esta info:
+// {
+//   "assets": [
+//     {
+//       "assetId": null,
+//       "base64": null,
+//       "duration": null,
+//       "exif": null,
+//       "height": 4800,
+//       "rotation": null,
+//       "type": "image",
+//       "uri": "file:///data/user/0/host.exp.exponent/cache/ExperienceData/%username%252Fsticker-smash-47-beta/ImagePicker/77c4e56f-4ccc-4c83-8634-fc376597b6fb.jpeg",
+//       "width": 3200
+//     }
+//   ],
+//   "canceled": false,
+//   "cancelled": false
+// }
